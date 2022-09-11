d87fc61b6d983f02d22d1a004ecb800f9dbb67fd
BarthelemyPavy <b.pavy@jellysmack.com>
2022-09-11 22:58:07 +0200

    🔧 update config files

---
+++ post_install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# add custom post-install commands below
+python -m nltk.downloader punkt
+python -m nltk.downloader stopwords
+python -m nltk.downloader wordnet
+python -m nltk.downloader averaged_perceptron_tagger
+python -m nltk.downloader omw-1.4
+python -m spacy download en
